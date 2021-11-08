ee4ee908e9509680e0f37d840917a77cd614905b
Basel-Ayman <eng_basel@icloud.com>
2021-11-09 00:47:02 +0200

    Update Algorithms.java
    Implement BFS

--- src/classes/Algorithms.java
+++ src/classes/Algorithms.java
@@ -3,77 +3,106 @@ package classes;
 import java.util.*;
 
 public class Algorithms {
-    private final  Long goal = 305419896L ;
+    private final Long goal = 305419896L;
     public Map<Long, Long> parents;
     public Set<Long> explored;
-    private  State state ;
+    private State state;
 
     public Algorithms() {
-        state = new State() ;
-    }
-    private  void swap ( int arr[][] , int i, int j , int x , int y  ){
-        int tmp = arr[i][j] ;
-        arr[i][j] = arr[x][y] ;
-        arr[x][y] = tmp ;
-    }
-    private Long getMove(int arr[][] , int i , int j , int x , int y ){
-        swap( arr , i, j , x, y );
-        Long s =   state.setStateLong( state.getStringFromTwoD(arr) )    ;
-        swap( arr , i, j , x , y );
-        return s ;
-    }
-    public   ArrayList<Long>  getNeighbors(){
-        ArrayList<Long> adj  = new ArrayList<>();
-        int[][] arr = state.formatToTwoD() ; // set zeroRow & zeroColumn .
-        if (   state.getZeroRow() > 0 ){
-            adj.add( getMove(arr , state.getZeroRow() , state.getZeroColumn() , state.getZeroRow() - 1 , state.getZeroColumn() ) )  ;
-        }
-        if ( state.getZeroColumn() > 0 ){
-            adj.add( getMove(arr , state.getZeroRow() , state.getZeroColumn() , state.getZeroRow()  , state.getZeroColumn()  - 1) )  ;
-        }
-        if ( state.getZeroRow() < 2  ){
-            adj.add( getMove(arr , state.getZeroRow() , state.getZeroColumn() , state.getZeroRow() + 1 , state.getZeroColumn() ) )  ;
-        }
-        if ( state.getZeroColumn() < 2 ){
-            adj.add( getMove(arr , state.getZeroRow() , state.getZeroColumn() , state.getZeroRow()  , state.getZeroColumn()  + 1) )  ;
-        }
-        return  adj ;
-    }
-    private void dfs(Long s ){
-         Stack <Long> st = new Stack<>() ;
-         explored.add(s) ;
-         st.push(s) ;
-         while( !st.empty() ){
-             Long x = st.pop() ;
-             if( x.equals(goal) ) break;
-             state.setCurrentState(x); // update the instance of state.
-             ArrayList<Long> adj = getNeighbors() ;
-             for(Long next : adj ){
-                 if(  !explored.contains(next) ){
-                     parents.put(next , x ) ;
-                     explored.add(next) ;
-                     st.push(next) ;
-                 }
-             }
-         }
+        state = new State();
     }
 
-    public Long getLong(String state){
+    private void swap(int arr[][], int i, int j, int x, int y) {
+        int tmp = arr[i][j];
+        arr[i][j] = arr[x][y];
+        arr[x][y] = tmp;
+    }
+
+    private Long getMove(int arr[][], int i, int j, int x, int y) {
+        swap(arr, i, j, x, y);
+        Long s = state.setStateLong(state.getStringFromTwoD(arr));
+        swap(arr, i, j, x, y);
+        return s;
+    }
+
+    public ArrayList<Long> getNeighbors() {
+        ArrayList<Long> adj = new ArrayList<>();
+        int[][] arr = state.formatToTwoD(); // set zeroRow & zeroColumn .
+        if (state.getZeroRow() > 0) {
+            adj.add(getMove(arr, state.getZeroRow(), state.getZeroColumn(), state.getZeroRow() - 1,
+                    state.getZeroColumn()));
+        }
+        if (state.getZeroColumn() > 0) {
+            adj.add(getMove(arr, state.getZeroRow(), state.getZeroColumn(), state.getZeroRow(),
+                    state.getZeroColumn() - 1));
+        }
+        if (state.getZeroRow() < 2) {
+            adj.add(getMove(arr, state.getZeroRow(), state.getZeroColumn(), state.getZeroRow() + 1,
+                    state.getZeroColumn()));
+        }
+        if (state.getZeroColumn() < 2) {
+            adj.add(getMove(arr, state.getZeroRow(), state.getZeroColumn(), state.getZeroRow(),
+                    state.getZeroColumn() + 1));
+        }
+        return adj;
+    }
+
+    private void dfs(Long s) {
+        Stack<Long> st = new Stack<>();
+        explored.add(s);
+        st.push(s);
+        while (!st.empty()) {
+            Long x = st.pop();
+            if (x.equals(goal))
+                break;
+            state.setCurrentState(x); // update the instance of state.
+            ArrayList<Long> adj = getNeighbors();
+            for (Long next : adj) {
+                if (!explored.contains(next)) {
+                    parents.put(next, x);
+                    explored.add(next);
+                    st.push(next);
+                }
+            }
+        }
+    }
+
+    public Long getLong(String state) {
         this.state.setStateLong(state);
-        return  this.state.getCurrentState() ;
-    }
-    public Stack<Long> DFS(Long initialState) {
-        initializeMapAndSet( initialState );
-        dfs(initialState) ;
-        return getPath(); 
+        return this.state.getCurrentState();
     }
 
-    // TODO:: Basel
+    public Stack<Long> DFS(Long initialState) {
+        initializeMapAndSet(initialState);
+        dfs(initialState);
+        return getPath();
+    }
+
     public Stack<Long> BFS(Long initialState) {
         initializeMapAndSet(initialState);
-        // TODO:: Implement BFS algorithm
-        return getPath();
+        // Queue to store frontier
+        Queue<Long> frontier = new LinkedList<Long>();
+        frontier.add(initialState);
 
+        while (!frontier.isEmpty()) {
+            Long stateLong = frontier.poll();
+            explored.add(stateLong);
+
+            // Stop if goal is reached
+            if (stateLong.equals(goal))
+                break;
+
+            state.setCurrentState(stateLong); // Update the instance of state
+            ArrayList<Long> neighbors = getNeighbors();
+            for (Long neighbor : neighbors) {
+                // Add neighbor states if only not in frontier & explored lists
+                if (!frontier.contains(neighbor) && !explored.contains(neighbor)) {
+                    parents.put(neighbor, stateLong);
+                    frontier.add(neighbor);
+                }
+            }
+        }
+        return getPath();
     }
 
     // TODO:: Man3am
@@ -98,23 +127,25 @@ public class Algorithms {
         return getPath();
 
     }
-    private void  initializeMapAndSet(Long initialState){
+
+    private void initializeMapAndSet(Long initialState) {
         // initialize them as every function will rewrite on them
-        parents =  new HashMap<Long,Long>();
+        parents = new HashMap<Long, Long>();
         explored = new HashSet<Long>();
-        parents.put(initialState , null )  ;
+        parents.put(initialState, null);
 
     }
 
     private Stack<Long> getPath() {
-        Stack<Long> path  = new Stack<>();
-        if ( !parents.containsKey(goal) ) return path ;
-         Long state = goal ;
-        while(state != null ){
-            path.push(state) ;
-            state = parents.get(state) ;
+        Stack<Long> path = new Stack<>();
+        if (!parents.containsKey(goal))
+            return path;
+        Long state = goal;
+        while (state != null) {
+            path.push(state);
+            state = parents.get(state);
         }
-        return path ;
+        return path;
     }
 
     // TODO:: Man3am
@@ -122,19 +153,21 @@ public class Algorithms {
 
         @Override
         public void call(State state) {
-            // TODO:: get ManhattanDistance between this sate and goal which defined in the top class
+            // TODO:: get ManhattanDistance between this sate and goal which defined in the
+            // top class
             // TODO :: h = abs(current cell:x - goal:x) + abs(current cell:y - goal:y)
 
         }
 
     }
-    
+
     // TODO:: Man3am
     private class EuclideanDistance implements Heuristics {
 
         @Override
         public void call(State state) {
-            // TODO:: get ManhattanDistance between this sate and goal which defined in the top class
+            // TODO:: get ManhattanDistance between this sate and goal which defined in the
+            // top class
             // TODO :: h = sqrt((current cell:x - goal:x)2+ (current cell.y - goal:y)2)
 
         }
